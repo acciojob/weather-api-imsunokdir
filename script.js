64a7e6e8d28fe51c4fa7787da106c58088a9848b
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2025-05-03 12:09:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,29 @@
-//your JS code here. If required.
+// your JS code here. If required.
 
 const btn = document.getElementById("btn");
 
 btn.addEventListener('click', getWeatherData);
 
-function getWeatherData(){
-	const city = 'London';
-	const api = '52bfc667ece724dfe46b5f0625a7ee93';
-	const apiUri = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`
+function getWeatherData() {
+	const city = 'London,uk';
+	const apiKey = '52bfc667ece724dfe46b5f0625a7ee93';
+	const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-	fetch(apiUri)
-	.then(res=>{
-		if(!res.ok){
-			throw new Error("Opps there was an error.")
-		}
-		return res.json();
-	})
-	.then(data=>{
-		const weatherDescription = data.weather[0].description;
-		const weatherDiv = document.getElementById("weatherData");
-		weatherDiv.textContent = `Current weather in ${city}: ${weatherDescription}`
-
-	})
-	.catch(error=>{
-		console.error('There was a problem with the fetch operation:', error);
-		const weatherDiv = document.getElementById('weatherData');
-        weatherDiv.textContent = 'Failed to fetch weather data. Please try again later.';
-	})
+	fetch(apiUrl)
+		.then(res => {
+			if (!res.ok) {
+				throw new Error("Oops, there was an error.");
+			}
+			return res.json();
+		})
+		.then(data => {
+			const weatherDescription = data.weather[0].description;
+			const weatherDiv = document.getElementById("weatherData");
+			weatherDiv.textContent = `Current weather in ${city}: ${weatherDescription}`;
+		})
+		.catch(error => {
+			console.error('There was a problem with the fetch operation:', error);
+			const weatherDiv = document.getElementById('weatherData');
+			weatherDiv.textContent = 'Failed to fetch weather data. Please try again later.';
+		});
 }
-
-
-
-
-
-
-
-
-
-
-
-
